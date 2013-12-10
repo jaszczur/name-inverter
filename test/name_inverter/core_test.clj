a871fdc5e516098b53c3e093a4570b24027e6b8c
Jaszczur <jaszczur20@interia.pl>
2013-12-10 17:46:05 +0100

    bsic case

--- test/name_inverter/core_test.clj
+++ test/name_inverter/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [name-inverter.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn invert-name [name]
+  (if (nil? name) "" name))
+
+(defn assert-inverted [name inverted]
+  (is (= (invert-name name) inverted)))
+
+(deftest invert-name-test
+  (testing "simple name"
+    (assert-inverted "Piotr" "Piotr"))
+  (testing "null name"
+    (assert-inverted nil "")))
