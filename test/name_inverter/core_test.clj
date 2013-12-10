714fe1de5aed0555d3c6c45770313f5548ed81a4
Jaszczur <jaszczur20@interia.pl>
2013-12-10 18:06:04 +0100

    trimming

--- test/name_inverter/core_test.clj
+++ test/name_inverter/core_test.clj
@@ -1,9 +1,12 @@
 (ns name-inverter.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [name-inverter.core :refer :all]))
 
 (defn invert-name [name]
-  (if (nil? name) "" name))
+  (if (nil? name)
+    ""
+    (str/trim name)))
 
 (defn assert-inverted [name inverted]
   (is (= (invert-name name) inverted)))
@@ -11,5 +14,9 @@
 (deftest invert-name-test
   (testing "simple name"
     (assert-inverted "Piotr" "Piotr"))
-  (testing "null name"
-    (assert-inverted nil "")))
+  (testing "given null should return empty string"
+    (assert-inverted nil ""))
+  (testing "given empty string should return empty string"
+    (assert-inverted "" ""))
+  (testing "given simple name with spaces should return it w/o spaces"
+    (assert-inverted " Piotr " "Piotr")))
